3025c82e0778a8f11318c316f856e4346338d73b
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-06-16 18:32:39 -0300

    chore: added constants

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = "ShortX";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://backend-teddy-ccyv.onrender.com";
