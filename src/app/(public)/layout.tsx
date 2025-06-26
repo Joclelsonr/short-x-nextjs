9f3224502c701de3fe7aa88c419624be5baccbba
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-06-26 16:09:30 -0300

    refactor: change path to redirect page

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import "../../global/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
