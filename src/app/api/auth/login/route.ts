9e092063c2e81869d7f542110213e3cd906e4648
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-06-27 12:37:44 -0300

    feat: creating login route with session

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
-import { API_URL } from "@/constants";
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { API_URL } from "@/constants";
 
 export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
@@ -18,7 +18,7 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  const { acceess_token } = await externalResponse.json();
+  const { acceess_token, user } = await externalResponse.json();
 
   const cookieStore = await cookies();
   cookieStore.set({
@@ -29,6 +29,12 @@ export async function POST(request: NextRequest) {
     sameSite: "lax",
     path: "/",
     maxAge: 60 * 60 * 24, // 1 day
+  });
+  cookieStore.set({
+    name: "user",
+    value: JSON.stringify(user),
+    httpOnly: false,
+    sameSite: "lax",
   });
 
   return NextResponse.json({ success: true });
